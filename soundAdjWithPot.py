b56fe38bab463c4f875d61fdbb20d1c11a58aee2
Baris Cakir <8ariscakir@gmail.com>
2024-01-23 00:53:21 +0300

    change variable names
    change "simdiki" to "value"

--- soundAdjWithPot.py
+++ soundAdjWithPot.py
@@ -9,38 +9,31 @@ from adafruit_hid.consumer_control_code import ConsumerControlCode
 import time
 keyboard = Keyboard(usb_hid.devices)
 cc = ConsumerControl(usb_hid.devices)
-onceki =0
-simdiki =0
+
+value =0
 
 a0=analogio.AnalogIn(board.GP27)
-print(a0.value)
 
 
-simdiki =int(a0.value/650)
+value =int(a0.value/650)
 perc=0
 for i in range(100):
     cc.send(ConsumerControlCode.VOLUME_DECREMENT)
-for y in range(simdiki):
+for y in range(value):
     cc.send(ConsumerControlCode.VOLUME_INCREMENT)
     perc=perc+1
-    
-print("simdiki")
-print(simdiki)
+
 
 while True:
-    #print(a0.value/650)
-    simdiki =int(a0.value/650)
+    value =int(a0.value/650)
     
-    if(perc < simdiki):
-        for c in range(simdiki-perc):
+    if(perc < value):
+        for c in range(value-perc):
             cc.send(ConsumerControlCode.VOLUME_INCREMENT)
             perc=perc+1
-    if(perc > simdiki):
-        for c in range(perc-simdiki):
+    if(perc > value):
+        for c in range(perc-value):
             cc.send(ConsumerControlCode.VOLUME_DECREMENT)
             perc=perc-1
-    
-    
-    #onceki=simdiki
-    
+
     time.sleep(0.1)
